6863ce1f970ae4b57c5ed766c59217ff7add06be
asthabh23 <asthabhargava001@gmail.com>
2024-01-19 17:11:59 +0530

    fix: image-list design

--- blocks/image-list/image-list.js
+++ blocks/image-list/image-list.js
@@ -1,27 +1,6 @@
-// export default function decorate(block) {
-//   const list = block.children;
-//   [...list].forEach((item) => {
-//     item.classList.add('item');
-//   });
-// }
-
 export default function decorate(block) {
-  const cols = [...block.firstElementChild.children];
-  block.classList.add(`image-list-${cols.length}-cols`);
-
-  // setup image columns
-  [...block.children].forEach((row) => {
-    [...row.children].forEach((col) => {
-      const pic = col.querySelector('picture');
-      if (pic) {
-        const picWrapper = pic.closest('div');
-        if (picWrapper && picWrapper.children.length === 1) {
-          // picture is only content in column
-          picWrapper.classList.add('image-list-img-col');
-        }
-      } else {
-        col.classList.add('image-list-text-col');
-      }
-    });
+  const list = block.children;
+  [...list].forEach((item) => {
+    item.classList.add('item');
   });
 }
